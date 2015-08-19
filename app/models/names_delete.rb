3257f941e463adf3ac5a37de7dae196a208600a8
Greg Clarke <gregbclarke@gmail.com>
2015-08-19 15:44:40 +1000

    NSL-1362: reasons for deleting a name - add to dropdown.

--- app/models/names_delete.rb
+++ app/models/names_delete.rb
@@ -27,7 +27,7 @@ class NamesDelete < ActiveType::Object
   validates :extra_info, presence: true, if: :reason_is_other?
 
   def possible_reasons
-    %w(YAGNI Other)
+    ["Name does not exist","Name has not been applied to Australian flora","Name is an autonym that has not yet been established", "Other"]
   end
 
   def assembled_reason
