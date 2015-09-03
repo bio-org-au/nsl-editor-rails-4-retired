e8245a0b8a9446237a71b306d568c9aceb188bfc
Greg Clarke <gregbclarke@gmail.com>
2015-09-03 11:21:48 +1000

    NSL-120: add test of validation to reject duplicate instances.

---
+++ test/models/instance/validations/duplicates/prevent_test.rb
@@ -0,0 +1,31 @@
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#   
+require 'test_helper'
+
+class InstanceValidationDuplicatesPreventTest < ActiveSupport::TestCase
+
+  test "instance prevent duplicate" do
+    instance = instances(:triodia_in_brassard)
+    assert instance.valid?, "Starting instance must be valid for this test."
+    dup = instance.dup
+    assert_raises(ActiveRecord::RecordInvalid,"Duplicate instance shouldn't be saved") do
+      dup.save!
+    end
+  end
+ 
+end
+
