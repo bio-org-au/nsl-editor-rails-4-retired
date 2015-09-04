f77c815670ffae5d7ff8d78709b635b9193a319f
Greg Clarke <gregbclarke@gmail.com>
2015-09-04 11:25:26 +1000

    NSL-817: tests for when you can offer the author delete button

---
+++ test/controllers/authors/tabs/for_editor/delete_widgets/offer_delete_button/if_can_be_deleted_test.rb
@@ -0,0 +1,33 @@
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#   
+require 'test_helper'
+
+class AuthorEditorOfferDeleteButtonIfCanBeDeletedTest < ActionController::TestCase
+  tests AuthorsController
+
+  test "should show editor author delete button if can be deleted" do
+    author = authors(:has_no_dependents)
+    @request.headers["Accept"] = "application/javascript"
+    assert author.can_be_deleted?, "Must be able to delete this author for the test to be valid"
+    get(:show,{id: author.id,tab: 'tab_edit'},{username: 'fred', user_full_name: 'Fred Jones', groups: ['edit']})
+    assert_select 'li.active a#author-edit-tab', 'Edit', "Should show 'Edit' tab."
+    assert_select 'a#author-delete-link', true
+  end
+
+end
+
+
---
+++ test/models/author/can_be_deleted/if_no_dependents_test.rb
@@ -0,0 +1,27 @@
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#   
+require 'test_helper'
+
+class AuthorCanBeDeletedIfNoDependentsTest < ActiveSupport::TestCase
+
+  test "author can be deleted if no dependents" do
+    author = authors(:has_no_dependents)
+    assert author.can_be_deleted?, "Should be able to delete author with no dependents"
+  end
+
+end
+
