e794e9eb8969e36f8e89e2401f7051bb32d7d8e2
Greg Clarke <gregbclarke@gmail.com>
2015-12-14 18:34:30 +1100

    NSL-1570: partial rubocop on instances controller.

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -13,34 +13,36 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#   
+#
 class InstancesController < ApplicationController
   include ActionView::Helpers::TextHelper
   before_filter :find_instance, only: [:show, :tab, :destroy]
 
   # GET /instances/1
   # GET /instances/1/tab/:tab
-  # Sets up RHS details panel on the search results page.  Displays a specified or default tab.
-  # ToDo: fix stick tabs to handle different tabs for standalone and relationships.
-  def show 
-    @tab = "#{ (params[:tab] && !params[:tab].blank? && params[:tab] != 'undefined') ? params[:tab] : 'tab_show_1' }"
-    @tab_index = (params[:tabIndex]||'1').to_i
+  # Sets up RHS details panel on the search results page.
+  # Displays a specified or default tab.
+  # ToDo: sticky tabs to handle different tabs for standalone
+  # and relationship instances.
+  def show
+    @tab = "#{(params[:tab] && !params[:tab].blank? && params[:tab] != 'undefined') ? params[:tab] : 'tab_show_1'}"
+    @tab_index = (params[:tabIndex] || '1').to_i
     @tabs_to_offer = tabs_to_offer
     render 'show', layout: false
   end
 
-  alias tab show
+  alias_method :tab, :show
 
   # Create the lesser version of relationship instance.
   def create_cited_by
     if instance_params[:name_id].blank?
       @instance = Instance.new
       @instance.errors.add(:base, 'You must choose a name.')
-      render 'create_error', locals: {focus_on_this_id: 'instance-name-typeahead'}
+      render 'create_error', locals: { focus_on_this_id: 'instance-name-typeahead' }
     elsif instance_params[:instance_type_id].blank?
       @instance = Instance.new
       @instance.errors.add(:base, 'You must choose an instance type.')
-      render 'create_error', locals: {focus_on_this_id: 'instance_instance_type_id'}
+      render 'create_error', locals: { focus_on_this_id: 'instance_instance_type_id' }
     else
       create
     end
@@ -51,26 +53,26 @@ class InstancesController < ApplicationController
     if instance_params[:cites_id].blank?
       @instance = Instance.new
       @instance.errors.add(:base, 'You must choose an instance.')
-      render 'create_error', locals: {focus_on_this_id: 'instance-instance-for-name-showing-reference-typeahead'}
+      render 'create_error', locals: { focus_on_this_id: 'instance-instance-for-name-showing-reference-typeahead' }
     elsif instance_params[:cited_by_id].blank?
       @instance = Instance.new
       @instance.errors.add(:base, 'Please refresh the tab.')
-      render 'create_error', locals: {focus_on_this_id: 'instance-instance-for-name-showing-reference-typeahead'}
+      render 'create_error', locals: { focus_on_this_id: 'instance-instance-for-name-showing-reference-typeahead' }
     elsif instance_params[:instance_type_id].blank?
       @instance = Instance.new
       @instance.errors.add(:base, 'You must choose an instance type.')
-      render 'create_error', locals: {focus_on_this_id: 'instance_instance_type_id'}
+      render 'create_error', locals: { focus_on_this_id: 'instance_instance_type_id' }
     else
       cites = Instance.find(instance_params[:cites_id])
       cited_by = Instance.find(instance_params[:cited_by_id])
-      the_params = {name_id: cites.name.id, 
-                    cites_id: cites.id, 
-                    cited_by_id: cited_by.id,
-                    reference_id: cited_by.reference.id,
-                    instance_type_id: instance_params[:instance_type_id],
-                    verbatim_name_string: instance_params[:verbatim_name_string],
-                    bhl_url: instance_params[:bhl_url],
-                    page: instance_params[:page]}
+      the_params = { name_id: cites.name.id,
+                     cites_id: cites.id,
+                     cited_by_id: cited_by.id,
+                     reference_id: cited_by.reference.id,
+                     instance_type_id: instance_params[:instance_type_id],
+                     verbatim_name_string: instance_params[:verbatim_name_string],
+                     bhl_url: instance_params[:bhl_url],
+                     page: instance_params[:page] }
       create(the_params)
     end
   end
@@ -90,7 +92,7 @@ class InstancesController < ApplicationController
   # PUT /instances/1.json
   def update
     @instance = Instance::AsEdited.find(params[:id])
-    @message = @instance.update_if_changed(instance_params,current_user.username)
+    @message = @instance.update_if_changed(instance_params, current_user.username)
     render 'update.js'
   rescue => e
     @message = e.to_s
@@ -99,7 +101,7 @@ class InstancesController < ApplicationController
 
   # PUT /instances/reference/1
   # PUT /instances/reference/1.json
-  # Changing the reference for an instance is a special case - 
+  # Changing the reference for an instance is a special case -
   # there may/will exist denormalised ids in dependent instances.
   # We have to temporarily bypass some validations to sort it out.
   def change_reference
@@ -108,7 +110,7 @@ class InstancesController < ApplicationController
     @instance.assign_attributes(instance_params)
     if @instance.changed?
       @instance_back_door = InstanceBackDoor.find(params[:id])
-      @instance_back_door.change_reference(instance_params,current_user.username)
+      @instance_back_door.change_reference(instance_params, current_user.username)
       @message = 'Updated'
     end
     render 'update.js'
@@ -122,7 +124,7 @@ class InstancesController < ApplicationController
   def destroy
     @instance.delete_as_user(current_user.username)
   rescue => e
-    logger.error("Instance#destroy exception: #{e.to_s}")
+    logger.error("Instance#destroy exception: #{e}")
     @message = e.to_s
     render 'destroy_error.js'
   end
@@ -134,49 +136,36 @@ class InstancesController < ApplicationController
 
   # Expect instance id - of the instance user is updating.
   def typeahead_for_name_showing_references_to_update_instance
-    references = []
-    unless params[:instance_id].blank?
-      references = Reference.find_by_sql(["select i.id,r.citation,r.year, r.pages, r.source_system, t.name instance_type from reference r  " +
-                                          " inner join author a on r.author_id = a.id " +
-                                          " inner join instance i on r.id = i.reference_id " +
-                                          " inner join instance_type t on i.instance_type_id = t.id " +
-                                          " where i.name_id = (select name_id from instance where id = ?)" +
-                                          "   and i.id != ? " +
-                                          "   and lower(r.citation) like lower('%'||?||'%') " +
-                                          " order by r.year,a.name",
-                                          params[:instance_id].to_i,params[:instance_id].to_i,params[:term]]).
-                                          collect { | ref | {value: "#{ref.citation}:#{ref.year} #{'['+ref.pages+']' unless ref.pages_useless?} #{'['+ref.instance_type+']' unless ref.instance_type == 'secondary reference'} #{'['+ref.source_system.downcase+']' unless ref.source_system.blank?}", id: ref.id}}
-    end
-    render json: references
+    typeahead = Instance::AsTypeahead::ForNameShowingReferences.new(params)
+    render json: typeahead.references
   end
- 
+
   # Copy an instance with its citations
   def copy_standalone
     current_instance = Instance::AsCopier.find(params[:id])
-    @instance = current_instance.copy_with_citations_to_new_reference(instance_params,current_user.username)
+    @instance = current_instance.copy_with_citations_to_new_reference(instance_params, current_user.username)
     @message = 'Instance was copied'
     render 'instances/copy_standalone/success.js'
   rescue => e
-    logger.error("There was a problem copying that instance: #{e.to_s}")
+    logger.error("There was a problem copying that instance: #{e}")
     @message = e.to_s
     render 'instances/copy_standalone/error.js'
   end
- 
- private 
-  
+
+  private
+
   def find_instance
     @instance = Instance.find(params[:id])
-    rescue ActiveRecord::RecordNotFound 
-      flash[:alert] = "We could not find the instance." 
-      redirect_to instances_path
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'We could not find the instance.'
+    redirect_to instances_path
   end
 
   def instance_params
-    params.require(:instance).permit(:instance_type, :name_id, :reference_id, 
-                   :instance_type_id, :verbatim_name_string, :page,
-                   :cites_id, :cited_by_id, :bhl_url)
+    params.require(:instance).permit(:instance_type, :name_id, :reference_id,
+                                     :instance_type_id, :verbatim_name_string, :page,
+                                     :cites_id, :cited_by_id, :bhl_url)
   end
-
 
   # Different types of instances require different sets of tabs.
   def tabs_to_offer
@@ -189,12 +178,9 @@ class InstancesController < ApplicationController
       offer << 'tab_apc_placement'
     end
     offer << 'tab_comments'
-    if @instance.simple? && params['row-type'] == 'instance_as_part_of_concept_record' 
+    if @instance.simple? && params['row-type'] == 'instance_as_part_of_concept_record'
       offer << 'tab_copy_to_new_reference'
     end
     offer
   end
-
-
 end
-
---
+++ app/models/instance/as_typeahead/for_name_showing_references.rb
@@ -0,0 +1,36 @@
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+class Instance::AsTypeahead::ForNameShowingReferences
+
+  attr_reader :references
+
+  def initialize(params)
+    @references = []
+    unless params[:instance_id].blank?
+      @references = Reference.find_by_sql(['select i.id,r.citation,r.year, r.pages, r.source_system, t.name instance_type from reference r  ' \
+                                          ' inner join author a on r.author_id = a.id ' \
+                                          ' inner join instance i on r.id = i.reference_id ' \
+                                          ' inner join instance_type t on i.instance_type_id = t.id ' \
+                                          ' where i.name_id = (select name_id from instance where id = ?)' \
+                                          '   and i.id != ? ' \
+                                          "   and lower(r.citation) like lower('%'||?||'%') " \
+                                          ' order by r.year,a.name',
+                                          params[:instance_id].to_i, params[:instance_id].to_i, params[:term]])
+                   .collect { |ref| { value: "#{ref.citation}:#{ref.year} #{'[' + ref.pages + ']' unless ref.pages_useless?} #{'[' + ref.instance_type + ']' unless ref.instance_type == 'secondary reference'} #{'[' + ref.source_system.downcase + ']' unless ref.source_system.blank?}", id: ref.id } }
+    end
+  end
+end
