f4ea234be5fa7ec4b1e092192b535d38f6c3fcfe
Greg Clarke <gregbclarke@gmail.com>
2015-12-14 14:44:25 +1100

    Rubocopped search controller.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,60 +13,42 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#   
+#
 class SearchController < ApplicationController
   before_filter :hide_details
 
   def search
-    if params[:query].present?
-      # translate services/search/link
-      if params[:query_field] = 'name-instances'
-        params[:query_target] = 'instances-for-name-id'
-        params[:query_string] = params[:query].sub(/id:/,'')
-      end
-      # params[:query_target] = params[:query_on] if params[:query_on].present?
-    end
-    if params[:query_string].present? || params[:query_target].present? 
-      if params[:query_target].present? && params[:query_target].match(/\Atrees*/i)
-        params[:query] = params[:query_string]
-        tree_search
-      else
-        params[:current_user] = current_user
-        params[:include_common_and_cultivar_session] = session[:include_common_and_cultivar] 
-        @search = Search::Base.new(params) 
-        save_search(@search)
-      end
-    else
-      @search = Search::Empty.new(params)
-    end
+    handle_old_style_params
+    run_tree_search || run_local_search || run_empty_search
   rescue => e
-    logger.error("SearchController::search exception: #{e.to_s}")
     params[:error_message] = e.to_s
     @search = Search::Error.new(params) unless @search.present?
     save_search(@search)
   end
 
   def tree
-    params[:query_field] = 'apc' if params[:query_field].blank?
-    params[:query] = Name.find_by(full_name: 'Plantae Haeckel').id if params[:query].blank?
+    set_tree_defaults
     @search = Search::Tree.new(params)
-    @ng_template_path = tree_ng_path('dummy').gsub(/dummy/,'')
+    @ng_template_path = tree_ng_path('dummy').gsub(/dummy/, '')
     logger.debug("@ng_template_path: #{@ng_template_path}")
     render 'trees/index'
   rescue => e
-    logger.error("SearchController::tree exception: #{e.to_s}")
+    logger.error("SearchController::tree exception: #{e}")
     params[:error_message] = e.to_s
-    @search = Search::Error.new(params) 
+    @search = Search::Error.new(params)
   end
 
   def search_name_with_instances
-    @search = Search::Base.new({'query_string' => "instances-for-name-id: #{params[:name_id]}"}) 
+    @search = Search::Base.new(
+      'query_string' => "instances-for-name-id: #{params[:name_id]}"
+    )
     render 'search'
   end
- 
+
   def set_include_common_and_cultivar
     logger.debug('set_include_common_and_cultivar')
-    session[:include_common_and_cultivar] = !session[:include_common_and_cultivar]
+    session[:include_common_and_cultivar] = \
+      !session[:include_common_and_cultivar]
   end
 
   def extras
@@ -76,26 +58,59 @@ class SearchController < ApplicationController
 
   private
 
-  def save_search(search)
+  def save_search(_search)
     session[:searches] ||= []
     session[:searches].push(@search.to_history)
-    if session[:searches].size > 5
-      session[:searches].shift
-    end
+    session[:searches].shift if session[:searches].size > 5
   end
 
   def tree_search
-    params[:query_field] = 'apc' if params[:query_field].blank?
-    params[:query] = Name.find_by(full_name: 'Plantae Haeckel').id if params[:query].blank?
+    set_tree_defaults
     @search = Search::Tree.new(params)
-    @ng_template_path = tree_ng_path('dummy').gsub(/dummy/,'')
-    logger.debug("@ng_template_path: #{@ng_template_path}")
+    @ng_template_path = tree_ng_path('dummy').gsub(/dummy/, '')
     render 'trees/index'
   rescue => e
-    logger.error("SearchController::tree exception: #{e.to_s}")
+    logger.error("SearchController::tree exception: #{e}")
     params[:error_message] = e.to_s
-    @search = Search::Error.new(params) 
+    @search = Search::Error.new(params)
   end
 
+  # translate services/search/link
+  def handle_old_style_params
+    return unless params[:query].present?
+    return unless params[:query_field] == 'name-instances'
+    params[:query_target] = 'instances-for-name-id'
+    params[:query_string] = params[:query].sub(/id:/, '')
+  end
+
+  def run_tree_search
+    return false unless params[:query_target].present?
+    return false unless params[:query_target].match(/\Atrees*/i)
+    params[:query] = params[:query_string]
+    tree_search
+    true
+  end
+
+  def run_local_search
+    return false unless params[:query_string].present?
+    params[:current_user] = current_user
+    params[:include_common_and_cultivar_session] = \
+      session[:include_common_and_cultivar]
+    @search = Search::Base.new(params)
+    save_search(@search)
+    true
+  end
+
+  def run_empty_search
+    @search = Search::Empty.new(params)
+  end
+
+  def set_tree_defaults
+    params[:query_field] = 'apc' if params[:query_field].blank?
+    params[:query] = plantae_haeckel if params[:query].blank?
+  end
+
+  def plantae_haeckel
+    Name.find_by(full_name: 'Plantae Haeckel').id
+  end
 end
-  
