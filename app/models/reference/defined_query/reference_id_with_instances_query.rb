a9cdc21f2a8292dad3c26f1917c2a42dede67f09
Greg Clarke <gregbclarke@gmail.com>
2016-09-26 15:14:14 +1000

    Refactor ref id with instances query. [rubocop: 396]

--- app/models/reference/defined_query/reference_id_with_instances_query.rb
+++ app/models/reference/defined_query/reference_id_with_instances_query.rb
@@ -18,8 +18,7 @@
 #   A defined query is one that the Search class knows about and may
 #   instantiate.
 class Reference::DefinedQuery::ReferenceIdWithInstancesQuery
-
-    attr_reader :results,
+  attr_reader :results,
               :limited,
               :common_and_cultivar_included,
               :has_relation,
@@ -28,9 +27,15 @@ class Reference::DefinedQuery::ReferenceIdWithInstancesQuery
               :show_csv,
               :total
 
-  def initialize(parsed_request, sort_key = 'name')
-    debug('start')
-    run_query(parsed_request.where_arguments, parsed_request.limit.to_i, sort_key)
+  def initialize(parsed_request, sort_key = "name")
+    @parsed_request = parsed_request
+    @search_string = parsed_request.where_arguments
+    @sort_key = sort_key
+    @limit = parsed_request.limit.to_i || 100
+    @results = []
+    run_query
+    #           parsed_request.limit.to_i,
+    #           sort_key)
   end
 
   def debug(s)
@@ -39,61 +44,60 @@ class Reference::DefinedQuery::ReferenceIdWithInstancesQuery
   end
 
   # Query is "Instances for ref id"
-  def run_query(search_string, limit = 100, order_by = "name",
-                show_instances = true)
-    debug("Start new ref_usages: search string: #{search_string};
-                 show_instances: #{show_instances};
-                 limit: #{limit}; order by: #{order_by}")
-    reference_id = search_string.to_i
-    @results = []
-    # But what if that reference no longer exists?
-    reference = Reference.find_by(id: reference_id)
-    unless reference.blank?
-      reference.display_as_part_of_concept
-      @count = 1
-      query = reference
-              .instances
-              .joins(:name)
-              .includes(name: :name_status)
-              .includes(:instance_type)
-              .includes(this_is_cited_by: [:name, :instance_type])
-      query = order_by == "page" ? query.ordered_by_page : query.ordered_by_name
-      query.each do |instance|
-        debug("Query loop.....")
-        if @count < limit
-          if instance.cited_by_id.blank?
-            @count += 1
-            if show_instances
-              instance.display_within_reference
-              @results.push(instance)
-              instance.is_cited_by.each do |cited_by|
-                @count += 1
-                cited_by.expanded_instance_type = cited_by.instance_type.name
-                @results.push(cited_by)
-                if @count > limit
-                  limited = true
-                  break
-                end
-              end
-              unless instance.cites_this.nil?
-                @results.push(instance.cites_this)
-                @count += 1
-                if @count > limit
-                  limited = true
-                  break
-                end
-              end
-            end
-          end
-        end
-        if @count > limit
-          limited = true
-          break
+  def run_query
+    find_reference
+    find_instances unless @reference.blank?
+  end
+
+  def find_reference
+    reference_id = @search_string.to_i
+    @reference = Reference.find_by(id: reference_id)
+  end
+
+  def built_query
+    query = @reference
+            .instances
+            .joins(:name)
+            .includes(name: :name_status)
+            .includes(:instance_type)
+            .includes(this_is_cited_by: [:name, :instance_type])
+    @sort_by == "page" ? query.ordered_by_page : query.ordered_by_name
+  end
+
+  def find_instances_for_ref
+    built_query.each do |instance|
+      if @count < @limit
+        if instance.cited_by_id.blank?
+          @count += 1
+          include_standalone_instance(instance)
+          include_synonym(instance) unless instance.cites_this.nil?
         end
       end
-      @results.unshift(reference)
+      break if @count > @limit
     end
+  end
+
+  def find_instances
+    @reference.display_as_part_of_concept
+    @count = 1
+    find_instances_for_ref
+    @results.unshift(@reference)
+    @limited = true if @count > @limit
     @results
   end
 
+  def include_standalone_instance(instance)
+    instance.display_within_reference
+    @results.push(instance)
+    instance.is_cited_by.each do |cited_by|
+      @count += 1
+      cited_by.expanded_instance_type = cited_by.instance_type.name
+      @results.push(cited_by)
+    end
+  end
+
+  def include_synonym(instance)
+    @results.push(instance.cites_this)
+    @count += 1
+  end
 end
