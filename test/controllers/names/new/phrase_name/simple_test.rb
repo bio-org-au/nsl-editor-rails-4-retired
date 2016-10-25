59525d94406d83bac66c8676a37c1d157b063c5f
Greg Clarke <gregbclarke@gmail.com>
2016-10-25 15:40:16 +1100

    NSL-2004: Basic controller tests for new names, including phrase names.

---
+++ test/controllers/names/new/phrase_name/simple_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+require "test_helper"
+
+# Single controller test.
+#  Started GET "/names/new?category=phrase&random_id=3617584730&tabIndex=107"
+class NamesNewPhraseNameSimpleTest < ActionController::TestCase
+  tests NamesController
+
+  test "editor should be able to start a new phrase name" do
+    @request.headers["Accept"] = "application/javascript"
+    @request.session["username"] = "fred"
+    @request.session["user_full_name"] = "Fred Jones"
+    @request.session["groups"] = ["edit"]
+    xhr(:get, :new,
+        { category: "phrase",
+          random_id: "123445",
+          tabIndex: "107" },
+        {},
+        xhr: true)
+    assert_response :success, "Cannot edit new phrase name in details tab"
+    assert_select("h4", /New Phrase Name/)
+  end
+end
---
+++ test/controllers/names/new_row/cultivar/simple_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+require "test_helper"
+
+# Single controller test.
+class NamesNewRowCultivarNameSimpleTest < ActionController::TestCase
+  tests NamesController
+
+  test "editor should be able to start a new row for a cultivar name" do
+    @request.headers["Accept"] = "application/javascript"
+    @request.session["username"] = "fred"
+    @request.session["user_full_name"] = "Fred Jones"
+    @request.session["groups"] = ["edit"]
+    xhr(:get, :new_row,
+        { type: "cultivar" },
+        {},
+        xhr: true)
+    assert_response :success, "Cannot start new row for a cultivar name"
+    assert_match(/search-results-table/,
+                 response.body.to_s,
+                 "Missing expected element")
+    assert_match(/names.new.category=cultivar/,
+                 response.body.to_s,
+                 "Missing expected element")
+  end
+end
