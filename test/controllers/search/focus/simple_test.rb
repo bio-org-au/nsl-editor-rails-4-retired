5782967042f5c3517baf530089bcf0a065a0296e
Greg Clarke <gregbclarke@gmail.com>
2017-01-04 11:55:36 +1100

    NSL-2118: Test to see if focus element is populated when focus-id is supplied.

---
+++ test/controllers/search/focus/simple_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+require "test_helper"
+
+# Single search controller test.
+class SearchNamesWithFocusSimpleTest < ActionController::TestCase
+  tests SearchController
+
+  setup do
+    @name = names(:angophora_costata)
+  end
+
+  test "search for a set of names and focus on one" do
+    get(:search,
+        { query_target: "name", query_string: "a", focus_id: @name.id },
+        username: "fred",
+        user_full_name: "Fred Jones",
+        groups: [])
+    assert_response :success
+    assert_select "#search-results-summary",
+                  /41 names of 41/,
+                  "Should find recs for simple search on 'a'"
+    assert_select "#focus-id[value='#{@name.id}']", {count: 1}, "One Focus"
+  end
+end
