794597aeb5f15889247e14265f09db9fcc9c1ac6
Greg Clarke <gregbclarke@gmail.com>
2017-10-24 16:28:23 +1100

    NSL-2361: Prevent author with an abbreviation being marked as a duplicate of an author without an abbreviation.

---
+++ test/models/author/validations/master_has_abbrev_if_needed/master_no_abbrev_dupe_has_abbrev_test.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+#   Copyright 2015 Australian National Botanic Gardens
+#
+#   This file is part of the NSL Editor.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+require "test_helper"
+
+# Single author model test.
+class MasterNoAbbrevDuplicateHasAbbrevTest < ActiveSupport::TestCase
+  test "master no abbrev duplicate has abbrev" do
+    master = authors(:master_without_abbrev)
+    dupe = authors(:duplicate_with_abbrev)
+    assert dupe.valid?, "Duplicate should be valid"
+    assert dupe.abbrev.present?, "Duplicate should have an abbreviation."
+    dupe.duplicate_of_id = master.id
+    assert_not dupe.valid?,
+      "Dupe w/ abbrev shouldn't be valid if master has no abbrev"
+  end
+end
