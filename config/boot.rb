ab0a52015f23ee1306c62b02aa948eabd2113e21
Greg Clarke <gregbclarke@gmail.com>
2019-02-01 17:43:29 +1100

    Stop using node for the execjs runtime. Try switching back to therubyrhino.

--- config/boot.rb
+++ config/boot.rb
@@ -4,4 +4,4 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 
 # make the ExecJs use NodeJs
- ENV['EXECJS_RUNTIME'] = 'Node'
+# ENV['EXECJS_RUNTIME'] = 'Node'
