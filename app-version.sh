4d0bd358641278a96c84abc35499ed16b767941b
Greg Clarke <gregbclarke@gmail.com>
2019-05-08 15:55:44 +1000

    Modify app-version.sh

--- app-version.sh
+++ app-version.sh
@@ -1,7 +1,1 @@
-# This is NOT the authoritative source of the app version.
-# The authoritative source of the app version is 
-# config/initializers/version.rb which is used to display the
-# running app version.
-#
-# This file is purely used in Jenkins for tagging the build.
 APP_VERSION=1.83
