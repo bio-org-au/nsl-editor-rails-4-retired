5872ecc3a33b482b3ba9c0a3f3681d25a86bb2bb
Greg Clarke <gregbclarke@gmail.com>
2019-05-09 09:59:33 +1000

    Reinstated the version.properties file (used by Jenkins) and added logic to config_initializers/version.rb to read the version from version.properties.

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,1 +1,16 @@
-Rails.configuration.version = '1.83'
+begin 
+  Rails.configuration.version_file_name = 'version.properties'
+  puts "Reading the app version from the #{Rails.configuration.version_file_name} file"
+  version='unknown'
+  a = File.readlines(Rails.configuration.version_file_name)
+  a.select {|line| line =~ /appversion/}
+  b = a.select {|line| line =~ /appversion/}
+  version = b.first.chomp.split('=').last
+  puts("Configuring version #{version} of the app.")
+  Rails.configuration.version = version
+rescue => e
+  puts e.to_s
+  puts "Warning: config couldn't find the app version from the expected file."
+  puts "Action: check #{Rails.configuration.version_file_name} exists and has an 'appversion=...' line"
+  Rails.configuration.version = 'unknown'
+end
