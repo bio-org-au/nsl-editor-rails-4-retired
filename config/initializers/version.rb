b1870e33712f02deb907d504ae3ab98d2d74302b
Greg Clarke <gregbclarke@gmail.com>
2019-05-09 10:38:57 +1000

    Moved version.properties to the config directory, following on from the previous commit.

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,8 +1,13 @@
+# Doing this because we want Jenkins to grab the app version for the build
+# processing and we need rails to display the version number in the app.
+#
+# Turns out, it was easier to use ruby to pull the info from a Jenkins-friendly 
+# file than for Jenkins to extract the info from a ruby file.
 begin 
-  Rails.configuration.version_file_name = 'version.properties'
-  puts "Reading the app version from the #{Rails.configuration.version_file_name} file"
+  Rails.configuration.version_file_path = 'config/version.properties'
+  puts "Reading the app version from the #{Rails.configuration.version_file_path} file"
   version='unknown'
-  a = File.readlines(Rails.configuration.version_file_name)
+  a = File.readlines(Rails.configuration.version_file_path)
   a.select {|line| line =~ /appversion/}
   b = a.select {|line| line =~ /appversion/}
   version = b.first.chomp.split('=').last
@@ -11,6 +16,6 @@ begin
 rescue => e
   puts e.to_s
   puts "Warning: config couldn't find the app version from the expected file."
-  puts "Action: check #{Rails.configuration.version_file_name} exists and has an 'appversion=...' line"
+  puts "Action: check #{Rails.configuration.version_file_path} exists and has an 'appversion=...' line"
   Rails.configuration.version = 'unknown'
 end
