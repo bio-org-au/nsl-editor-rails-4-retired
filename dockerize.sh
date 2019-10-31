366b932d41b79f634058287cf2877e7dbe36d05a
pmcneil <peter@nerderg.com>
2019-10-31 14:14:08 +1100

    Add a new travis.yml build config move docker repo to biodiversity

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 docker build -t nsl-editor .
-docker tag nsl-editor pmcneil/nsl-editor:1.88-SNAP
-docker push pmcneil/nsl-editor:1.88-SNAP
+docker tag nsl-editor biodiversity/nsl-editor:1.88-SNAP
+docker push biodiversity/nsl-editor:1.88-SNAP
