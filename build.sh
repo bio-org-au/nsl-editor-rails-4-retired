ea2feb1f67c71bf18b02a08434b1a70b7726ce98
pmcneil <peter@nerderg.com>
2019-11-04 12:44:53 +1100

    See if we can use rvm to do jruby build in travis This comments out the jruby download.

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-JRUBY_ZIP=bin/jruby-dist-9.1.12.0-bin.zip
-export JAVA_OPTS='-server -d64'
-export JRUBY_HOME=./bin/jruby-9.1.12.0
-
-if [ ! -d "$JRUBY_HOME" ]; then
-  if [ ! -f "$JRUBY_ZIP" ]; then
-    curl https://repo1.maven.org/maven2/org/jruby/jruby-dist/9.1.12.0/jruby-dist-9.1.12.0-bin.zip --output $JRUBY_ZIP
-  fi
-  unzip -d bin $JRUBY_ZIP
-fi
-
-export PATH=$JRUBY_HOME/bin:$PATH
+#JRUBY_ZIP=bin/jruby-dist-9.1.12.0-bin.zip
+#export JAVA_OPTS='-server -d64'
+#export JRUBY_HOME=./bin/jruby-9.1.12.0
+#
+#if [ ! -d "$JRUBY_HOME" ]; then
+#  if [ ! -f "$JRUBY_ZIP" ]; then
+#    curl https://repo1.maven.org/maven2/org/jruby/jruby-dist/9.1.12.0/jruby-dist-9.1.12.0-bin.zip --output $JRUBY_ZIP
+#  fi
+#  unzip -d bin $JRUBY_ZIP
+#fi
+#
+#export PATH=$JRUBY_HOME/bin:$PATH
 export EDITOR_CONFIG_FILE=editor-build-config.rb
 export EDITOR_CONFIGDB_FILE=editor-build-database.yml
 
@@ -28,8 +28,8 @@ echo "** remove existing war files"
 rm ./*.war || echo "no war files"
 
 echo "** gem install"
-jruby bin/jruby-9.1.12.0/bin/gem install bundler
-jruby bin/jruby-9.1.12.0/bin/bundle install --without development test
+jruby -S gem install bundler
+jruby -S bundle install --without development test
 
 echo "** compile assets"
 bundle exec rake assets:clobber
